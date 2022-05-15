520ae5d570936203e8475721dcbe8c047754e67f
ThoaiVo <vokythoai@gmail.com>
2022-05-15 17:45:09 +0700

    Implement layout for blog

--- src/components/header/menu_header.js
+++ src/components/header/menu_header.js
@@ -1,47 +1,66 @@
 import './menu_header.css';
-import { Container, Menu, MenuButton, MenuList, MenuItem, Button, Box, Image } from '@chakra-ui/react';
+import {
+  Flex,
+  Container,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+  Box,
+  Image
+} from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
+import { FaFacebook, FaAirbnb, FaStackpath } from 'react-icons/fa';
 
 function MenuHeader() {
   return (
-    <Container className="menu-header">
-      <Box className="menu-header__menu-icon">
-        <Image src="" alt="" />
-      </Box>
-      <Box className="menu-header__menu-bar">
-        
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-            Projects
-          </MenuButton>
+    <Container className="menu-header" p={6} mt={6}>
+      <Flex>
+        <Button colorScheme="whiteAlpha" leftIcon={<FaStackpath />}></Button>
 
-          <MenuList>
-            <MenuItem> Project_1 </MenuItem>
-            <MenuItem> Project 2 </MenuItem>
-            <MenuItem> Project 3 </MenuItem>
-            <MenuItem> Project 4 </MenuItem>
-            <MenuItem> Project 5 </MenuItem>
-          </MenuList>
-        </Menu>
+        <Box className="menu-header__menu-icon">
+          <Image src="" alt="" />
+        </Box>
 
-        <Menu>
-          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}> 
-            Blogs 
-          </MenuButton>
-          <MenuList>
-            <MenuItem> Data Structure </MenuItem>
-            <MenuItem> Algorithm </MenuItem>
-            <MenuItem> Architecture Design </MenuItem>
-            <MenuItem> Database </MenuItem>
-            <MenuItem> Ultilities </MenuItem>
-          </MenuList>
-        </Menu>
-      </Box>
+        <Box className="menu-header__menu-bar">
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Projects
+            </MenuButton>
 
-      <Box className="menu-header__menu-button">
-        <Button> Sign In </Button>
-        <Button> Get Started </Button>
-      </Box>
+            <MenuList>
+              <MenuItem> Project_1 </MenuItem>
+              <MenuItem> Project 2 </MenuItem>
+              <MenuItem> Project 3 </MenuItem>
+              <MenuItem> Project 4 </MenuItem>
+              <MenuItem> Project 5 </MenuItem>
+            </MenuList>
+          </Menu>
+
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              Blogs
+            </MenuButton>
+            <MenuList>
+              <MenuItem> Data Structure </MenuItem>
+              <MenuItem> Algorithm </MenuItem>
+              <MenuItem> Architecture Design </MenuItem>
+              <MenuItem> Database </MenuItem>
+              <MenuItem> Ultilities </MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+
+        <Box className="menu-header__menu-button">
+          <Button colorScheme="facebook" leftIcon={<FaFacebook />}>
+            Sign In
+          </Button>
+          <Button colorScheme="twitter" leftIcon={<FaAirbnb />}>
+            Get Started
+          </Button>
+        </Box>
+      </Flex>
     </Container>
   );
 }
--- src/components/new_feed/new_feed_item.js
+++ src/components/new_feed/new_feed_item.js
@@ -1,20 +1,15 @@
-import {
-  Box,
-  Container,
-  Heading,
-  Image
-} from '@chakra-ui/react';
+import { Box, Container, Heading, Image } from '@chakra-ui/react';
 
 function NewFeedItem(props) {
   return (
-    <Container className="new-feed-item"> 
+    <Container className="new-feed-item">
       <Box>
         <Image src={props.image} />
         <Heading>{props.title}</Heading>
         <Heading>{props.short_desciprtion}</Heading>
       </Box>
     </Container>
-  )
+  );
 }
 
 export default NewFeedItem;
